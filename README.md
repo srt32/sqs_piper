3158ec31c6ac95353b4bced4a8b11450b91336c9
Simon Taranto <simon@globality.com>
2016-08-24 16:17:28 -0400

    use url direclty

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A utility to receive an AWS SQS message from one queue and then send it on
 another queue
 
-`go run main.go <source queue name> <sink source name>`
+`AWS_ACCOUNT_ID=121749107756 AWS_SDK_LOAD_CONFIG=true go run main.go crespi-queue-dev crespi-queue-simon`
 
 A potential use case is to pull a message from a remote queue and pipe it into
 a queue you listen on locally for debugging purposes.
--- main.go
+++ main.go
@@ -4,40 +4,48 @@ import (
 	"log"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 func main() {
 	if len(os.Args) != 3 {
-		log.Fatalln("Usage:", os.Args[0], "<source_queue> <sink_queue>")
+		log.Fatalln("Usage:", os.Args[0], "<source_queue_url> <sink_queue_url>")
 	}
 
+	// awsAccountId := os.Getenv("AWS_ACCOUNT_ID")
+
+	sourceUrl := os.Args[1]
+	sinkUrl := os.Args[2]
+
+	log.Println("Source url:", sourceUrl)
+	log.Println("Sink url:", sinkUrl)
+
 	sess, err := session.NewSessionWithOptions(session.Options{
-		Config:  aws.Config{Region: aws.String("us-east-1")},
+		// Config:  aws.Config{Region: aws.String("us-east-1")},
 		Profile: "development",
 	})
 
 	svc := sqs.New(sess)
-	sourceUrlRes, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
-		QueueName:              aws.String(os.Args[1]),
-		QueueOwnerAWSAccountId: aws.String("121749107756"),
-	})
-	if err != nil {
-		log.Fatalln("Source GetQueueURL failed:", err)
-	}
+	//sourceUrlRes, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+	//	QueueName:              aws.String(os.Args[1]),
+	//	QueueOwnerAWSAccountId: aws.String(awsAccountId),
+	//})
+	//if err != nil {
+	//	log.Fatalln("Source GetQueueURL failed:", err)
+	//}
 
-	sinkUrlRes, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
-		QueueName:              aws.String(os.Args[2]),
-		QueueOwnerAWSAccountId: aws.String("121749107756"),
-	})
-	if err != nil {
-		log.Fatalln("Sink GetQueueURL failed:", err)
-	}
+	//sinkUrlRes, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+	//	QueueName:              aws.String(os.Args[2]),
+	//	QueueOwnerAWSAccountId: aws.String(awsAccountId),
+	//})
+	//if err != nil {
+	//	log.Fatalln("Sink GetQueueURL failed:", err)
+	//}
 
+	// TODO: increase wait time
 	receiveResponse, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
-		QueueUrl: sourceUrlRes.QueueUrl,
+		QueueUrl: &sourceUrl,
 	})
 	if err != nil {
 		log.Fatalln("ReceiveMessage failed:", err)
@@ -47,7 +55,7 @@ func main() {
 
 	_, err = svc.SendMessage(&sqs.SendMessageInput{
 		MessageBody: message.Body,
-		QueueUrl:    sinkUrlRes.QueueUrl,
+		QueueUrl:    &sinkUrl,
 	})
 	if err != nil {
 		log.Fatalln("SendMessage failed:", err)
